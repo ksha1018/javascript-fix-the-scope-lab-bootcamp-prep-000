ae42bf81dabf873cd88946c9b19ae08224a6606f
ksha1018@naver.com <ksha1018@naver.com>
2018-03-01 08:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,16 +21,20 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return '"FUNKY!"';
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
+
+
 console.log(myAnimal());
 console.log(yourAnimal());
 console.log(add2('animal is '));
 var theFunk = funkyFunction();
 console.log(theFunk());
+
+
 
 
